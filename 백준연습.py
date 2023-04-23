fbd07b080328882f547f5860750daecd8bfbaaad
Heen_A <104770890+LimHeena@users.noreply.github.com>
2023-04-23 23:34:34 +0900

    add practice file 0423
    백준 풀이 0423

---
+++ 백준연습.py
@@ -0,0 +1,25 @@
+#230422
+
+#11720번
+# n = int(input())
+# nums = str(input())
+# sum = 0
+# for i in range(n):
+#     sum += int(nums[i])
+# print(sum)
+
+#2675번
+# n = int(input())
+# resultlist = []
+# for i in range(n):
+#     mylist=[]
+#     mylist1=[]
+#     myin = input().split()
+#     num = myin[0]
+#     sen = myin[1]
+#     for i in range(len(sen)):
+#         mylist1.append(sen[i]*int(num))
+#     result = ''.join(mylist1)
+#     resultlist.append(result)
+# for i in range(len(resultlist)):
+#     print(resultlist[i], end='\n')
