22c23fd3e47a0541d671c1b20de5f5979e3c84f6
Heen_A <104770890+LimHeena@users.noreply.github.com>
2023-04-30 14:13:54 +0900

    add coding file 0429
    백준 풀이 0429

--- BAEKJOON/백준연습.py
+++ BAEKJOON/백준연습.py
@@ -1,69 +1,76 @@
-#230424
+#230429
 
-#1712번 - 이렇게 풀면 시간초과
-# a,b,c = map(int, input().split())
-# num = 1
-# if b >= c:
-#     print(-1)
-# else:
-#     while True:
-#         if a + b*num < c*num:
-#             break
-#         else:
-#             num += 1
-#     print(num)
-#정답
-# a,b,c = map(int, input().split())
-# if b >= c:
-#     print(-1)
-# else:
-#     print(a//(c-b)+1)
+#11654번
+#내가 시도한.....
+# w = input()
+# mydict={}
+# a=48
+# for i in range(10):
+#     mydict[i] = a
+#     a+=1
+# b=65
+# Alpha=['A','B','C','D','E']
+# for i in range(25):
 
-#17614번
-# num = int(input())
-# ct = 0
-# for i in range(1, num+1):
-#     for j in str(i):
-#         if j =='3' or j == '6' or j == '9':
-#             ct += 1
-# print(ct)
+#ord() : 문자의 아스키 코드값을 리턴하는 함수이다.
+#chr() : 아스키 코드값 입력으로 받아 그 코드에 해당하는 문자를 출력하는 함수이다.
 
-#2884번
-# h,m = map(int, input().split())
-# m = m-45
-# if m < 0:
-#     m = 60+m
-#     h = h-1
-#     if h < 0:
-#         h = 24+h
-# print(h, m)
-
-#2444번
-# n = int(input())
-# for i in range(1,n+1):
-#     print(' '*(n-i)+'*'*(2*i-1))
-# for j in range(n-1,0,-1):
-#     print(' '*(n-j)+'*'*(2*j-1))
+# a = input()
+# print(ord(a))
 
 #2908번
-# a,b = map(str, input().split())
-# a = a[-1]+a[-2]+a[-3]
-# b = b[-1]+b[-2]+b[-3]
-# a = int(a)
-# b = int(b)
+# a,b=input().split()
+# a=a[-1]+a[-2]+a[-3]
+# b=b[-1]+b[-2]+b[-3]
+# a=int(a)
+# b=int(b)
 # print(max(a,b))
 
-#3040번 
-#브루트포스 알고리즘
-# mylist=[]
-# for i in range(9):
-#     mylist.append(int(input()))
-# for j in range(9):
-#     for k in range(j+1, 9):
-#         if sum(mylist) - mylist[j] - mylist[k] == 100:
-#             a,b = j,k
-#             break
-# mylist.pop(a)
-# mylist.pop(b-1)
-# for i in range(7):
-#     print(mylist[i])
+#15552번
+# import sys
+# n =int(input())
+# for i in range(n):
+#     a,b = map(int, sys.stdin.readline().split())
+#     print(a+b)
+
+#10162번
+# n = int(input())
+# a=300; b=60; c=10
+# A=0; B=0; C=0
+# no = False
+# if n//a > 0:
+#     A = n//a
+#     n=n-(300*A)
+# else:
+#     A=0
+# if n//b > 0:
+#     B = n//b
+#     n=n-(60*B)
+# else:
+#     B=0
+# if n//c > 0 and n%c==0:
+#     C = n//c    
+# elif (A>0 or B>0) and n%c==0:
+#     no = False
+# else:
+#     no = True
+#     print(-1)
+# if no == False:
+#     print(A, end=' ')
+#     print(B, end=' ')
+#     print(C, end=' ')
+
+#5618번 - 실패
+# n = int(input())
+# if n == 2:
+#     a,b = map(int, input().split())
+#     maxnum = max(a,b)
+#     for i in range(1,maxnum+1):
+#         if a%i==0 and b%i==0:
+#             print(i)
+# else:
+#     a,b,c = map(int, input().split())
+#     maxnum = max(a,b,c)
+#     for i in range(1,maxnum+1):
+#         if a%i==0 and b%i==0:
+#             print(i)
