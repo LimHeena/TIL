87f60a51a272d77d5d8816b0e7cc8df3b397a834
Heen_A <104770890+LimHeena@users.noreply.github.com>
2023-07-25 23:57:35 +0900

    algorithm 230723
    algorithm 230723

---
+++ Practice/연습용 파일.py
@@ -0,0 +1,25 @@
+# 1주차 1
+
+# n = input()
+# s = input()
+# result = s.swapcase()
+# print(result)
+
+# 1주차 2
+
+# import sys
+# input = sys.stdin.readline
+# s, e = map(int, input().split())
+# S = input().rstrip()
+# E = input().rstrip()
+# while S in E:
+# 	E = E.replace(S, '')
+# if E:
+# 	print(E)
+# else:
+# 	print('EMPTY')
+
+# 1주차 3
+
+# one, two = input().split()
+# print(max(eval(one), eval(two)))
